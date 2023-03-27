91b5c70eb30c064f8f8760ffe181f0d4f6ebeca3
fajar <crackspace770>
2023-03-27 09:02:25 +0700

    firebase cloud messaging app.

---
+++ app/src/main/java/com/example/firebasechat/Message.kt
@@ -0,0 +1,12 @@
+package com.example.firebasechat
+
+
+data class Message (
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val timestamp: Long? = null
+        )
+{
+
+}
